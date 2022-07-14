20c99ac454c1b4f6887241ca7d22523aecec03db
Kiara Mckenzie <108815461+KiarMcke29@users.noreply.github.com>
2022-07-14 18:40:56 -0400

    Add files via upload

---
+++ generator/main.js
@@ -0,0 +1,20 @@
+//const setBg = () => {
+   // const randomColor = 
+   // Math.floor(Math.random()*16777215).toString(16);
+   // document.body.style.backgroundColor = "#" + randomColor;
+   // color.innerHTML = "#" + randomColor;
+//  }
+  
+  //genNew.addEventListener("click", setBg);
+//function randomFruit()  {
+//let fruits = new array();
+
+    
+       // function blockImage1()  {
+        //  let targetDiv = document.getElementById("demo1")
+        //  if (targetDiv.style.display !== "none") {
+        //    targetDiv.style.display = "none";
+        //  } else {
+         //   targetDiv.style.display = "block";
+         // }
+      //  }
